2e6d644f96c7edb8768bce851dd031275de7976f
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2024-03-04 22:00:13 -0500

    prueba 2 embarques

--- public/index.php
+++ public/index.php
@@ -102,7 +102,7 @@ $router = new Router();
 
     //API para los embarques
     $router->get('/api/embarque', [embarqueController::class, 'index']);
-    $router->get('/crear/embarque', [embarqueController::class, 'crear']);
+    $router->get('/crear/embarque', [EmbarqueController::class, 'crear']);
     $router->post('/crear/embarque', [embarqueController::class, 'crear']);
     $router->post('/api/embarque/actualizar', [embarqueController::class, 'actualizar']);
     $router->post('/api/embarque/eliminar', [embarqueController::class, 'eliminar']);
